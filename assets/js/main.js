793a42423d633daf08e1a318398e3e71c50d6f7f
Mrh Ripon <mrhripon98@gmail.com>
2020-06-03 23:44:39 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    "use strict";
+
+
+
+
+
+
+
+
+    // Nice Select Activation 
+    $(document).ready(function () {
+        $('select').niceSelect();
+    })
+
+
+
+    $("#verticle-scroll").niceScroll({
+        cursorcolor: "aquamarine",
+        cursorwidth: "5px",
+        background: "#181F32",
+        cursorborderradius: "5px",
+        cursorcolor: "#fff",
+    });
+
+    $("#horizontal-scroll").niceScroll({
+        cursorcolor: "aquamarine",
+        cursorwidth: "5px",
+        background: "#181F32",
+        cursorborderradius: "5px",
+        cursorcolor: "#fff",
+    });
+
+    $("#horizontal-scroll2").niceScroll({
+        cursorcolor: "aquamarine",
+        cursorwidth: "5px",
+        background: "#181F32",
+        cursorborderradius: "5px",
+        cursorcolor: "#fff",
+    });
+
+
+
+
+
+
+
+});
